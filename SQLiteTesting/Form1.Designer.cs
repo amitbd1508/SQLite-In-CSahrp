0fffcea3a809100f3360f60db51527b57fdde7b0
Amit Ghosh <amitbd1508@gmail.com>
2017-09-08 22:26:21 +0600

    inital commit

---
+++ SQLiteTesting/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace SQLiteTesting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.TxtPrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbType = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rtxt = new System.Windows.Forms.RichTextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid
+            // 
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Location = new System.Drawing.Point(12, 308);
+            this.grid.Name = "grid";
+            this.grid.Size = new System.Drawing.Size(742, 335);
+            this.grid.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(119, 22);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(264, 26);
+            this.txtName.TabIndex = 7;
+            // 
+            // TxtPrice
+            // 
+            this.TxtPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPrice.Location = new System.Drawing.Point(119, 116);
+            this.TxtPrice.Name = "TxtPrice";
+            this.TxtPrice.Size = new System.Drawing.Size(133, 26);
+            this.TxtPrice.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(67, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(68, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Type";
+            // 
+            // cmbType
+            // 
+            this.cmbType.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Items.AddRange(new object[] {
+            "Appetizer Gallery",
+            "Continental Gallery",
+            "Fast Food Gallery",
+            "Lounge Special Set Menu",
+            "Rice & Pasta Gallery",
+            "Soups & Salads Gallery",
+            "Thai & Chinese Gallery"});
+            this.cmbType.Location = new System.Drawing.Point(119, 69);
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(264, 28);
+            this.cmbType.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Serve With";
+            // 
+            // rtxt
+            // 
+            this.rtxt.Location = new System.Drawing.Point(117, 156);
+            this.rtxt.Name = "rtxt";
+            this.rtxt.Size = new System.Drawing.Size(271, 139);
+            this.rtxt.TabIndex = 13;
+            this.rtxt.Text = "";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(407, 258);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(347, 37);
+            this.btnInsert.TabIndex = 14;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(407, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(347, 227);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 657);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.rtxt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbType);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.grid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox TxtPrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbType;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox rtxt;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SQLiteTesting/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLiteTesting
+{
+    public partial class Form1 : Form
+    {
+        Bitmap img;
+        string conn;
+        SQLiteConnection sqlconn;
+        private SQLiteDataAdapter DB;
+        private DataSet DS = new DataSet();
+        private DataTable DT = new DataTable();
+        private DataGrid Grid;
+        public Form1()
+        {
+
+            InitializeComponent();
+            
+            cmbType.DropDownStyle = ComboBoxStyle.DropDownList;
+            MessageBox.Show("You are using 30days! trial version !!!!");
+            conn = @"Data Source =resturent.db; Version=3;New=False;Compress=True;";
+            img = new Bitmap(SQLiteTesting.Properties.Resources.icon_choose);
+            pictureBox1.Image = img;
+            sqlconn = new SQLiteConnection(conn);
+            try
+            {
+                sqlconn.Open();
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            selectAndFill();
+            
+        }
+       
+        
+       
+		
+        private void selectAndFill()
+        {
+            string ct = "select * from products";
+            DB = new SQLiteDataAdapter(ct, sqlconn);
+            DS.Reset();
+            DB.Fill(DS);
+            DT = DS.Tables[0];
+            grid.DataSource = DT;
+        }
+        void insert()
+        {
+
+            
+            string sql;
+            if (pictureBox1.Image != img)
+            {
+                
+                //sql = @"insert into products (name, description,price,type,image) values ('" + txtName.Text + "', '" + rtxt.Text + TxtPrice.Text + "', '" + cmbType.SelectedItem + "');";
+               // sql = @"insert into products (name, description,price,type,image) values ('" + txtName.Text + "', '" + rtxt.Text + "', '" + TxtPrice.Text + "', '" + cmbType.SelectedItem + "', '" + bitmapToString(new Bitmap(pictureBox1.Image)) + "');";
+                //MessageBox.Show(sql);
+                var data = ImageToByteArray(pictureBox1.Image);
+                using (var cmd = new SQLiteCommand("INSERT INTO products (name, description,price,type,image) values(@name,@description,@price,@type, @image)", sqlconn))
+                {
+                    cmd.Parameters.Add("@image", DbType.Binary).Value = data;
+                    cmd.Parameters.Add("@name", DbType.String).Value = txtName.Text;
+                    cmd.Parameters.Add("@description", DbType.String).Value = rtxt.Text;
+                    cmd.Parameters.Add("@price", DbType.String).Value = TxtPrice.Text;
+                    cmd.Parameters.Add("@type", DbType.String).Value = cmbType.Text;
+              
+                    cmd.ExecuteNonQuery();
+                    selectAndFill();
+                    pictureBox1.Image = img;
+                    return;
+                }
+                
+            }
+            else
+                sql = @"insert into products (name, description,price,type) values ('" + txtName.Text + "', '" + rtxt.Text + "', '" + TxtPrice.Text + "', '" + cmbType.SelectedItem + "');";
+            //MessageBox.Show(sql);
+            SQLiteCommand ss = new SQLiteCommand(sql, sqlconn);
+            ss.ExecuteNonQuery();
+            selectAndFill();
+            pictureBox1.Image = img;
+            
+        }
+        public static byte[] ImageToByteArray(Image imageIn)
+        {
+            var ms = new MemoryStream();
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+            return ms.ToArray();
+        }
+
+        public static Image ByteArrayToImage(byte[] byteArrayIn)
+        {
+            var ms = new MemoryStream(byteArrayIn);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Image";
+                dlg.Filter = "All Files|*.*";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    
+                    pictureBox1.Image = new Bitmap(dlg.FileName);
+
+                    
+                }
+            }
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            if (checkPrice()&&txtName.Text != "" && TxtPrice.Text != "" )
+            {
+                insert();
+                pictureBox1.Image = img;
+                rtxt.Text = "";
+                txtName.Text = "";
+                TxtPrice.Text = "";
+            }
+            else MessageBox.Show("Check Name ,Type and Price ");
+          
+        }
+
+        private bool checkPrice()
+        {
+
+            try
+            {
+                decimal i = Convert.ToDecimal(TxtPrice.Text.ToString());
+                return true;
+
+            }
+            catch
+            {
+               
+                return false;
+                
+            }
+            
+        }
+    }
+}
